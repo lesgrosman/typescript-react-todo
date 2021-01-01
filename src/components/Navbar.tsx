96e5995d223c57948897182cf4f2e7f85f1efdb0
Glib <andreyagasov@gmail.com>
2021-01-01 18:28:40 +0100

    bugs fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Navbar: React.FC = () => (
     <nav>
         <div className="nav-wrapper blue darken-1">
-            <a href="/" className="brand-logo logo">My Todo App</a>
+            <span className="brand-logo logo">My Todo App</span>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <a href="https://github.com/lesgrosman/typescript-react-todo"><i className="fa fa-github fa-2x git"></i></a>
             </ul>
